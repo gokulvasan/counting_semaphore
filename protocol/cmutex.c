65defd98e6b4f914a4fa1e0fa55925078ff89cfc
gokulvasan <gokulvas@gmail.com>
2016-01-16 20:42:34 +0100

    Structural Change
    Made a seperate  folder to segregate protocol and application.

--- protocol/cmutex.c
+++ protocol/cmutex.c
@@ -1,17 +1,17 @@
 #include "cmutex.h"
 
 void cmutex_init(struct cmutex* mutex,
-		prologue_epilogue_fn prologue,
-		prologue_epilogue_fn epilogue) {
+		entry_exit_fn prologue,
+		entry_exit_fn epilogue) {
 
 	if(!mutex)
 		return;
 
    	pthread_mutex_init(&mutex->res_mgr, NULL);
    	pthread_mutex_init(&mutex->crit_sec, NULL);
-	mutex->prologue = prologue;
-	mutex->epilogue = epilogue;
-
+	mutex->prologue = entry;
+	mutex->epilogue = exit;
+	
 	return;
 }
 
@@ -19,7 +19,7 @@ void cmutex_proberen(struct cmutex* mutex, void *params) {
 
 	while(!pthread_mutex_lock(&mutex->res_mgr)) {
 		/*
-		* if the prologue and epilogue is NULL then
+		* if the entry and exit is NULL then
 		* just use a single mutex with normal 
 		* functionality. 
 		*/
@@ -42,13 +42,13 @@ void cmutex_proberen(struct cmutex* mutex, void *params) {
 void cmutex_verhogen(struct cmutex* mutex, void *params) 
 {
 	/*
-	* If prologue is null then it should act like
+	* If entry is null then it should act like
 	* a normal mutex functionality with wrapper.
 	*/
 	if(!mutex->prologue) {
 		pthread_mutex_unlock(&mutex->res_mgr);
 		if(mutex->epilogue)
-			mutex->epilogue(params);
+			mutex->exit(params);
 	}
 
 	pthread_mutex_lock(&mutex->crit_sec);
